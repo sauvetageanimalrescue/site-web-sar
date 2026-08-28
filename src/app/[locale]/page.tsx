8896f87c86b574ad852753249e89116cdf8b59e1
Eric Dussault <e.dussault@sar.quebec>
2026-08-28 11:21:35 -0400

    Hero d'accueil: monter encore 100px, tasser encore 300px vers la droite

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -49,7 +49,7 @@ function Hero() {
           // révéler moins de largeur de la même photo, jamais un zoom.
           // Au-delà de 2489px de large, le fond marine prend le relais.
           backgroundSize: "2489px 1400px",
-          backgroundPosition: "calc(50% - 100px) calc(35% - 100px)",
+          backgroundPosition: "calc(50% - 400px) calc(35% - 200px)",
           backgroundRepeat: "no-repeat",
         }}
       />

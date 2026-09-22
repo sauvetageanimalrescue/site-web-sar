cabb348b4ae14f993cccd1aefa0cba88a9aa9e08
Eric Dussault <e.dussault@sar.quebec>
2026-09-22 14:54:42 -0400

    Placer le localisateur de refuges dans Organisation

--- src/lib/menu.ts
+++ src/lib/menu.ts
@@ -25,6 +25,7 @@ export const MENU: SectionMenu[] = [
       { cle: "serie", href: "/serie-televisee" },
       { cle: "medias", href: "/medias" },
       { cle: "faq", href: "/foire-aux-questions" },
+      { cle: "localisateurRefuges" },
     ],
   },
   {
